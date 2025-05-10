0e1e0c5a3b0a7a9e42a432c285a71bc6df0b15cb
Hafidh <hafidhhusna@mail.ugm.ac.id>
2025-05-10 21:08:50 +0700

    feat : update api routes for pagination

--- app/api/items/route.ts
+++ app/api/items/route.ts
@@ -2,10 +2,27 @@ import {prisma} from "@/lib/prisma";
 import { stat } from "fs";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(){
+export async function GET(req: NextRequest){
     try{
-        const items = await prisma.item.findMany()
-        return NextResponse.json(items)
+        const {searchParams} = new URL(req.url);
+        const page = parseInt(searchParams.get("page") || "1", 10);
+        const limit = parseInt(searchParams.get("limi") || "10", 10);
+
+        const skip = (page-1) * limit;
+
+        const items = await prisma.item.findMany({
+            skip,
+            take:limit,
+        })
+
+        const totalItems = await prisma.item.count();
+        const totalPages = Math.ceil(totalItems / limit);
+
+        return NextResponse.json({
+            items,
+            totalPages,
+            currentPage : page,
+        })
     } catch(error){
         console.error(error);
         return NextResponse.json({error : "Gagal Memuat Data"}, {status : 500});
