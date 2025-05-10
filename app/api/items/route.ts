6dd01e060d2e8ad97be044208ab403742143dca4
Hafidh <hafidhhusna@mail.ugm.ac.id>
2025-05-10 20:40:35 +0700

    initial commit

---
+++ app/api/items/route.ts
@@ -0,0 +1,38 @@
+import {prisma} from "@/lib/prisma";
+import { stat } from "fs";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(){
+    try{
+        const items = await prisma.item.findMany()
+        return NextResponse.json(items)
+    } catch(error){
+        console.error(error);
+        return NextResponse.json({error : "Gagal Memuat Data"}, {status : 500});
+    }
+}
+
+export async function POST(req : NextRequest){
+    try{
+        const {name, quantity} = await req.json();
+
+        if(!name || !quantity || quantity <= 0){
+            return NextResponse.json(
+                {error : "Nama Barang Wajib diisi dan Jumlah Harus Angka Positif"},
+                {status : 400}
+            )
+        }
+
+        const newItem = await prisma.item.create({
+            data : {
+                name,
+                quantity,
+            },
+        })
+
+        return NextResponse.json(newItem, {status : 201});
+    } catch (error){
+        console.error(error);
+        return NextResponse.json({error : "Gagal mencatat barang"}, {status : 500})
+    }
+}
